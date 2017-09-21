b31cf1eb19e006f3617d8c19f7734e6b7495f970
thiagopitu <32144785+thiagopitu@users.noreply.github.com>
2017-09-20 21:18:24 -0300

    Add files via upload

--- exercicio5atualizado.cpp
+++ exercicio5atualizado.cpp
@@ -9,13 +9,14 @@ setlocale (LC_ALL, "Portuguese");
 	int nota5 = 5, nota10 = 10, nota50 = 50, nota100 = 100;
 	int resto1, resto2, resto3, resto4;
 	int a, b, c, d, e;
+	e = a + b + c + d;
 	
 	
 	printf ("Possíveis notas:\nR$5\nR$10\nR$50\nR$100\n\n");
 	printf ("Indique o valor de saque: ");
+	scanf ("%i", saque);
 	
 	/* 
-                                       
        700(saque)/nota100= 7 (a)
 	   700(saque)%nota100= 80(resto4)
 	   
@@ -24,8 +25,6 @@ setlocale (LC_ALL, "Portuguese");
 	   
 	   30(resto3)/nota10= 3 (c)
 	   30(resto3)%nota10= 0(resto2)
-	   
-	
 	*/
 	
 	
@@ -40,7 +39,7 @@ setlocale (LC_ALL, "Portuguese");
 	  
 	  d = resto2/nota5;
 	  resto1 = 0;
-             printf ("Quantidade de notas usadas: %i", a+b+c+d\n\n);
+             printf ("Quantidade de notas usadas: %i", e);
              printf ("%i notas de 5 reais, %i notas de 10 reais, %i notas de 50 reais, %i notas de 100 reais.", a, b, c, d);
 	
 	
